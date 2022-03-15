e42ece014534de1827bf3fa2a02090ac801043a4
init.defaultBranch <benlimpic@gmail.com>
2022-03-15 09:10:59 -0700

    commit tree 6d931d16662e1bd10ff8f7e4b0d3bb6c4bc20116

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+const currentUser = "Ben Limpic";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
+
+
+
+
